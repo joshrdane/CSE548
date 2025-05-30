c8839421e4c94ae5dca6eb70ba4ab6527836e17d
Joshua Dane <joshrdane@gmail.com>
2025-05-30 05:14:44 -0700

    Project 1 initial commit

---
+++ Project 1/config.client.sh
@@ -0,0 +1,10 @@
+# Set static IP address on client nat network
+nmcli connection modify "Wired connection 1" ipv4.method manual
+nmcli connection modify "Wired connection 1" ipv4.address 10.0.2.20/24
+nmcli connection modify "Wired connection 1" ipv4.gateway 10.0.2.10
+
+# Delete the default route
+route del default
+
+# Set the default route to the Gateway/Server VM
+route add default gw 10.0.2.20 dev enp0s3
