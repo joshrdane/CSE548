f5a6657622fa7ac3ffe1a0838a757fe7fdb922f1
Joshua Dane <joshrdane@gmail.com>
2025-05-31 21:19:51 -0700

    install nmap on client

--- Project 1/config.client.sh
+++ Project 1/config.client.sh
@@ -1,3 +1,6 @@
+# Install required packages
+apt install nmap -y
+
 # Set static IP address on client nat network
 nmcli connection modify "Wired connection 1" ipv4.method manual
 nmcli connection modify "Wired connection 1" ipv4.address 10.0.2.20/24
