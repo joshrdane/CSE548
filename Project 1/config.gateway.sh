597ebaff3b30b1a8808f684d690fb592db8809b4
Joshua Dane <joshrdane@gmail.com>
2025-05-31 21:32:47 -0700

    configure index page earlier

--- Project 1/config.gateway.sh
+++ Project 1/config.gateway.sh
@@ -4,6 +4,9 @@ apt update && apt upgrade -y
 # Install Apache
 apt install apache2 -y
 
+# Configure index page
+echo "Welcome to the Packet Filter Firewall (iptables) Project demo and test page!" > /var/www/html/index.html
+
 # Set static IP address on client nat network
 nmcli connection modify "Wired connection 1" ipv4.method manual
 nmcli connection modify "Wired connection 1" ipv4.address 10.0.2.10/24
@@ -14,9 +17,6 @@ nmcli connection modify "Wired connection 2" ipv4.address 10.0.1.10/24
 
 # Configure ip forwarding
 echo "1" > /proc/sys/net/ipv4/ip_forward
-
-# Configure index page
-echo "Welcome to the Packet Filter Firewall (iptables) Project demo and test page!" > /var/www/html/index.html
 
 # Use rc.firewall script to configure iptables
 ./rc.firewall
